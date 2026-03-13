39cefe98eaccbc1abe7a5d59bae1d89111ae33f7
brian <briancheruiyot5347@gmail.com>
2026-03-13 20:02:48 +0300

    Week 4 AI assignment

--- assignment.py
+++ assignment.py
@@ -1,1 +1,23 @@
+## question 4
+print("Hello Ai Students") 
 
+## question 5
+person = {
+"name": "Brian",
+"age": 20,
+"city": "Nairobi"}
+print(person["name"])
+print(person["age"])
+print(person["city"]) 
+
+## question 6
+Language =["Python","JavaScript","Java","C++","Go"]
+print(Language[2])
+
+##question 7
+Footballer ={"name": "Brian",
+             "age": 23,
+             "position": "striker"}
+print(Footballer["name"])
+print(Footballer["age"])
+print(Footballer["position"])
